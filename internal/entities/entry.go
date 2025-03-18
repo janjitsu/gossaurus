dd0ebbaef6fa596810fe8a3aae226a88e5811365
Jan Silva <janfrs3@gmail.com>
2025-03-18 12:07:17 +0100

    feat: Use gocsv to parse entries

--- internal/entities/entry.go
+++ internal/entities/entry.go
@@ -1,22 +1,31 @@
 package entities
 
+import "strings"
+
+type StringList []string
+
+func (list *StringList) UnmarshalCSV(csv string) (err error) {
+	*list = strings.Split(csv, ",")
+	return nil
+}
+
 type Entry struct {
-	Title      string
-	Inflexions []string
-	Grammar1   string
-	Meaning1   string
-	Example1   string
-	Grammar2   string
-	Meaning2   string
-	Example2   string
-	Grammar3   string
-	Meaning3   string
-	Example3   string
-	Grammar4   string
-	Meaning4   string
-	Example4   string
-	Note1      string
-	Note2      string
-	Note3      string
-	Note4      string
+	Title      string     `csv:"title"`
+	Inflexions StringList `csv:"inflexions"`
+	Grammar1   string     `csv:"grammar_1"`
+	Meaning1   string     `csv:"meaning_1"`
+	Example1   string     `csv:"example_1"`
+	Grammar2   string     `csv:"grammar_2"`
+	Meaning2   string     `csv:"meaning_2"`
+	Example2   string     `csv:"example_2"`
+	Grammar3   string     `csv:"grammar_3"`
+	Meaning3   string     `csv:"meaning_3"`
+	Example3   string     `csv:"example_3"`
+	Grammar4   string     `csv:"grammar_4"`
+	Meaning4   string     `csv:"meaning_4"`
+	Example4   string     `csv:"example_4"`
+	Note1      string     `csv:"note_1"`
+	Note2      string     `csv:"note_2"`
+	Note3      string     `csv:"note_3"`
+	Note4      string     `csv:"note_4"`
 }
--- internal/parser/parser.go
+++ internal/parser/parser.go
@@ -3,10 +3,11 @@ package parser
 import (
 	"encoding/csv"
 	"fmt"
+	"github.com/gocarina/gocsv"
 	"github.com/janjitsu/gossaurus/internal/entities"
+	"io"
 	"log"
 	"os"
-	"strings"
 )
 
 type CreateDictionaryParams struct {
@@ -18,53 +19,24 @@ type CreateDictionaryParams struct {
 	Cover       string
 }
 
-func parseTSV(filename string) [][]string {
-	fmt.Println(filename)
-	csvFile, _ := os.Open(filename)
-	r := csv.NewReader(csvFile)
-	r.Comma = '\t'         // Set tab as delimiter
-	r.FieldsPerRecord = -1 // Allow variable number of fields
-
-	// Read all records
-	records, err := r.ReadAll()
+func parseEntries(filename string) []entities.Entry {
+	csvFile, err := os.Open(filename)
 	if err != nil {
 		log.Fatalf("Failed to read TSV %v reason: %v", filename, err)
 		panic(err)
 	}
-	return records
-}
+	defer csvFile.Close()
+	gocsv.SetCSVReader(func(in io.Reader) gocsv.CSVReader {
+		r := csv.NewReader(in)
+		r.LazyQuotes = true
+		r.Comma = '\t'
+		r.FieldsPerRecord = -1 // Allow variable number of fields
+		return r               // Allows use dot as delimiter and use quotes in CSV
+	})
 
-func parseEntries(filename string) []entities.Entry {
-	records := parseTSV(filename)
-	//@TODO need a validator, tell me if some line is wrong and not crash the app
 	var entries []entities.Entry
-	for i, record := range records {
-		if i == 0 {
-			continue // Skip header
-		}
-		//@TODO this is ugly
-		var entry entities.Entry
-		entry.Title = record[0]
-		//@TODO if there are no inflexsions, still is setting an empty string
-		//@TODO empty fields have a dash ( //-) for better readability of the file, filter out this
-		entry.Inflexions = strings.Split(record[1], ",")
-		entry.Grammar1 = record[2]
-		entry.Meaning1 = record[3]
-		entry.Example1 = record[4]
-		entry.Grammar2 = record[5]
-		entry.Meaning2 = record[6]
-		entry.Example2 = record[7]
-		entry.Grammar3 = record[8]
-		entry.Meaning3 = record[9]
-		entry.Grammar4 = record[10]
-		entry.Meaning4 = record[11]
-		entry.Example4 = record[12]
-		entry.Note1 = record[13]
-		entry.Note2 = record[14]
-		entry.Note3 = record[15]
-		entry.Note4 = record[16]
-
-		entries = append(entries, entry)
+	if err := gocsv.UnmarshalFile(csvFile, &entries); err != nil {
+		panic(err)
 	}
 
 	return entries
